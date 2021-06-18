38a7e89764740fcaf513124b3127aba697d558c7
Jourofant <Jourofant@foxmail.com>
2021-06-18 17:43:06 +0800

    声明测试方法

--- Assets/Script/Builder/WorldBuilder.cs
+++ Assets/Script/Builder/WorldBuilder.cs
@@ -32,14 +32,26 @@ public class WorldBuilder : Singleton<WorldBuilder>
 
     private void BuildingChunk(Chunk chunk, Vector3 start) 
     {
+
+    }
+
+    /// <summary>
+    /// 测试用区块生成方法
+    /// </summary>
+    /// <param name="chunk">传入的区块信息</param>
+    /// <param name="start">起始位置坐标</param>
+    private void TestBuildingChunk(Chunk chunk, Vector3 start) 
+    {
         GameObject chunkRoot = new GameObject();
         chunkRoot.name = "Chunk 0 (x:0, y:0)";
         Vector3 curStart = Vector3.zero;
-        for (int i = 0; i < Chunk.Height; i++) {
-            curStart = new Vector3(curStart.x, i*16, curStart.z);
-            print("=================初始化测试分区： " + (i+1) + "号=================");
+        for (int i = 0; i < Chunk.Height; i++)
+        {
+            curStart = new Vector3(curStart.x, i * 16, curStart.z);
+            print("=================初始化测试分区： " + (i + 1) + "号=================");
             print("分区坐标为：x-" + curStart.x + "，y-" + curStart.y + "，z-" + curStart.z);
-            if (!chunk.ActiveList[i]) {
+            if (!chunk.ActiveList[i])
+            {
                 print("当前分区为空分区不执行生成");
                 continue;
             }
@@ -48,20 +60,24 @@ public class WorldBuilder : Singleton<WorldBuilder>
             sectionRoot.transform.SetParent(chunkRoot.transform);
             sectionRoot.name = "Section " + i;
             CubeType[,,] curCubeList = chunk.sectionsList[i].cube;
-            for (int y = 0; y < Section.Length; y++) {
+            for (int y = 0; y < Section.Length; y++)
+            {
                 GameObject planeRoot = new GameObject();
                 planeRoot.transform.SetParent(sectionRoot.transform);
                 planeRoot.name = "Plane " + y;
-                for (int x = 0; x < Section.Length; x++) {
-                    for (int z = 0; z < Section.Length; z++ ){
+                for (int x = 0; x < Section.Length; x++)
+                {
+                    for (int z = 0; z < Section.Length; z++)
+                    {
                         CubeType curCubeType = curCubeList[x, y, z];
-                        if (curCubeType == CubeType.Air) {
+                        if (curCubeType == CubeType.Air)
+                        {
                             continue;
                         }
                         string curPath = CubePath.Instance.GetPath(curCubeType);
                         GameObject curObj = Resources.Load<GameObject>(curPath);
                         Transform transform = Instantiate(curObj).transform;
-                        transform.position = new Vector3(curStart.x+x, curStart.y+y, curStart.z+z);
+                        transform.position = new Vector3(curStart.x + x, curStart.y + y, curStart.z + z);
                         transform.SetParent(planeRoot.transform);
                     }
                 }
