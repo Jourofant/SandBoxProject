8d3277d1686c9e8309f8c93ba087d60e30c92573
Jourofant <Jourofant@foxmail.com>
2021-06-19 18:35:41 +0800

    9区块生成测试

--- Assets/Script/Builder/WorldBuilder.cs
+++ Assets/Script/Builder/WorldBuilder.cs
@@ -25,9 +25,22 @@ public class WorldBuilder : Singleton<WorldBuilder>
     private void Awake()
     {
         // 测试用区块生成
-        Chunk Test = new Chunk(horizon);
+        //Chunk Test = new Chunk(horizon);
+        //Vector3 testStart = Vector3.zero;
+        //TestBuildingChunk(Test, testStart);
+
+        //测试多区块生成，9
+        Chunk[] Test = new Chunk[9];
         Vector3 testStart = Vector3.zero;
-        TestBuildingChunk(Test, testStart);
+        int i = 0;
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int z = -1; z <= 1; z++, i++)
+            {
+                Test[i] = new Chunk(horizon);
+                TestBuildingChunk(Test[i], new Vector3(testStart.x+(x*Section.Length), testStart.y, testStart.z+(z*Section.Length)), i);
+            }
+        }
     }
 
     private void BuildingChunk(Chunk chunk, Vector3 start) 
@@ -38,16 +51,16 @@ public class WorldBuilder : Singleton<WorldBuilder>
     /// <summary>
     /// 测试用区块生成方法
     /// </summary>
-    /// <param name="chunk">传入的区块信息</param>
+    /// <param name="chunk">区块信息</param>
     /// <param name="start">起始位置坐标</param>
-    private void TestBuildingChunk(Chunk chunk, Vector3 start) 
+    private void TestBuildingChunk(Chunk chunk, Vector3 start, int n) 
     {
         GameObject chunkRoot = new GameObject();
-        chunkRoot.name = "Chunk 0 (x:0, y:0)";
-        Vector3 curStart = Vector3.zero;
+        chunkRoot.name = "Chunk " + n + " (x:" + start.x +", z:" + start.z + ")";
+        Vector3 curStart;
         for (int i = 0; i < Chunk.Height; i++)
         {
-            curStart = new Vector3(curStart.x, i * 16, curStart.z);
+            curStart = new Vector3(start.x, i * 16, start.z);
             print("=================初始化测试分区： " + (i + 1) + "号=================");
             print("分区坐标为：x-" + curStart.x + "，y-" + curStart.y + "，z-" + curStart.z);
             if (!chunk.ActiveList[i])
@@ -84,4 +97,22 @@ public class WorldBuilder : Singleton<WorldBuilder>
             }
         }
     }
+
+    /// <summary>
+    /// 设置地平线高度，范围[1,255]
+    /// </summary>
+    /// <param name="n">地平线高度</param>
+    /// <returns>操作成功则为true</returns>
+    public bool SetHorizon(int n ) {
+        if (n > 0 && n < 256)
+        {
+            horizon = n;
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+
 }
